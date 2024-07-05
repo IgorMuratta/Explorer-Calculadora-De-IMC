369dbacc35d1ac6c75871c3a88bcd6d0fa3601bd
Igor Muratta <igormuratta@gmail.com>
2024-07-05 12:59:31 -0300

    update of the first interface but the alert card

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let weight = document.querySelector("#peso");   // Peso
+let height = document.querySelector("#altura"); // Altura
+
+
+addEventListener("click", (event) =>{
+    event.preventDefault();
+
+})
